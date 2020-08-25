aa3d808e995c5d79bba9e04022a902a5232ee380
Zach S.L <zach.s.ledford@gmail.com>
2020-08-25 14:07:35 -0400

    adding basic layout for app

---
+++ instagram-reels/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__top">
+        <img
+          className="app__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"
+          alt=""
+        />
+        <h1>Reels</h1>
+        {/* Reels text */}
+      </div>
+
+      <div className="app__videos">
+        {/* Container of app__videos */}
+        {/* <Video /> */}
+        {/* <Video /> */}
+        {/* <Video /> */}
+        {/* <Video /> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
